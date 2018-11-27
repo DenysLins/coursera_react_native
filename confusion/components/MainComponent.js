0d723a3e7826bc2a52a0fe2c426b055ced58fe7d
Denys Lins <denyslins@gmail.com>
2018-11-26 22:02:14 -0300

    Components Part 2

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import Dishdetailt from './DishdetailComponent';
 
 class Main extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            selectedDish: null
         };
+    }
+
+    onDishSelected(dishId) {
+        this.setState({ selectedDish: dishId })
     }
 
     render() {
 
         return (
-            <Menu dishes={this.state.dishes} />
+            <View>
+                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelected(dishId)} />
+                <Dishdetailt dish={this.state.dishes.filter(dish => dish.id === this.state.selectedDish)[0]} />
+            </View>
+
         );
     }
 }
