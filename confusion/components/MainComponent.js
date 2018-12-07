6cf2b7004ab17d63ad239ed659068b1ed5e86096
Denys Lins <denyslins@gmail.com>
2018-12-06 22:30:31 -0300

    Favorites

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -5,6 +5,7 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -123,6 +124,23 @@ const ReservationNavigator = createStackNavigator({
         })
     });
 
+const FavoritesNavigator = createStackNavigator({
+    Favorites: { screen: Favorites }
+}, {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"
+            },
+            headerTintColor: "#fff",
+            headerLeft: <Icon name="menu" size={24}
+                iconStyle={{ color: 'white' }}
+                onPress={() => navigation.navigate('DrawerToggle')} />
+        })
+    });
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -202,6 +220,22 @@ const MainNavigator = createDrawerNavigator({
                     color={tintColor}
                 />
             )
+        }
+    },
+    Favorites:
+    {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            title: 'My Favorites',
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                    name='heart'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
         }
     },
     Reservation:
