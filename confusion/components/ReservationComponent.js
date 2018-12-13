76bf8766e977f050aa46f974288600dbb1ccf8de
Denys Lins <denyslins@gmail.com>
2018-12-13 00:01:13 -0300

    Assignment 1 - task 2

--- confusion/components/ReservationComponent.js
+++ confusion/components/ReservationComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, ScrollView, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
-import { Permissions, Notifications } from 'expo';
+import { Permissions, Calendar } from 'expo';
 
 class Reservation extends Component {
 
@@ -28,32 +28,66 @@ class Reservation extends Component {
         });
     }
 
-    async obtainNotificationPermission() {
-        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+    handleReservation(date) {
+        this.addReservationToCalendar(date);
+    }
+
+    // async obtainNotificationPermission() {
+    //     let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+    //     if (permission.status !== 'granted') {
+    //         permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+    //         if (permission.status !== 'granted') {
+    //             Alert.alert('Permission not granted to show notifications');
+    //         }
+    //     }
+    //     return permission;
+    // }
+
+    async obtainCalendarPermission() {
+        let permission = await Permissions.getAsync(Permissions.CALENDAR);
         if (permission.status !== 'granted') {
-            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            permission = await Permissions.askAsync(Permissions.CALENDAR);
             if (permission.status !== 'granted') {
-                Alert.alert('Permission not granted to show notifications');
+                Alert.alert('Permission not granted to access calendar');
             }
         }
         return permission;
     }
 
-    async presentLocalNotification(date) {
-        await this.obtainNotificationPermission();
-        Notifications.presentLocalNotificationAsync({
-            title: 'Your Reservation',
-            body: 'Reservation for ' + date + ' requested',
-            ios: {
-                sound: true
-            },
-            android: {
-                sound: true,
-                vibrate: true,
-                color: '#512DA8'
+    async addReservationToCalendar(date) {
+        await this.obtainCalendarPermission();
+        Calendar.createEventAsync(
+            Calendar.DEFAULT,
+            {
+                title: 'Con Fusion Table Reservation',
+                startDate: new Date(Date.parse(date)),
+                endDate: new Date(Date.parse(date) + (2 * 60 * 60 * 1000)),
+                timeZone: 'Asia/Hong_Kong',
+                location: '121, Clear Water Bay Road, Clear Water Bay, Kowloon, Hong Kong'
             }
+        ).then(() => {
+            Alert.alert('Reservation added to calendar');
+        }).catch(error => {
+            console.log(error);
+            Alert.alert('Error while adding reservation to calendar');
         });
     }
+
+    // async presentLocalNotification(date) {
+    //     await this.obtainNotificationPermission();
+    //     Notifications.presentLocalNotificationAsync({
+    //         title: 'Your Reservation',
+    //         body: 'Reservation for ' + date + ' requested',
+    //         ios: {
+    //             sound: true
+    //         },
+    //         android: {
+    //             sound: true,
+    //             vibrate: true,
+    //             color: '#512DA8'
+    //         }
+    //     });
+    // }
 
     render() {
         return (
@@ -64,7 +98,7 @@ class Reservation extends Component {
                         <Picker
                             style={styles.formItem}
                             selectedValue={this.state.guests}
-                            onValueChange={(itemValue, itemIndex) => this.setState({ guests: itemValue })}>
+                            onValueChange={(itemValue) => this.setState({ guests: itemValue })}>
                             <Picker.Item label="1" value="1" />
                             <Picker.Item label="2" value="2" />
                             <Picker.Item label="3" value="3" />
@@ -124,7 +158,7 @@ class Reservation extends Component {
                                         {
                                             text: 'OK',
                                             onPress: () => {
-                                                this.presentLocalNotification(this.state.date);
+                                                this.handleReservation(this.state.date);
                                                 this.resetForm();
                                             }
                                         }
